759d2aa83bbad7bb0326660b92d5c563c80c06d9
rockBravo2 <00001116438707sp@al.educacao.sp.gov.br>
2025-09-02 10:32:52 -0300

    mig

--- script.js
+++ script.js
@@ -14,4 +14,14 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = '${tamanhoAtualFonte}rem'
 
     })
+
+
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+botaoDeAcessibilidade.addEventListener('click', function(){
+   botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+   opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+})
+
 })

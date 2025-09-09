16deed13d7ad20c8215b7e81447e7570c5b9ef0b
rockBravo2 <00001116438707sp@al.educacao.sp.gov.br>
2025-09-09 10:50:56 -0300

    ssws

--- script.js
+++ script.js
@@ -28,4 +28,7 @@ document.addEventListener('DOMContentLoaded', function(){
     alternaContraste.addEventListener('click', function()){
 document.body.classList.toggle('alto-contarste');
     })
+
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado);
 })

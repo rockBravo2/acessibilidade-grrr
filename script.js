42da933b0dbd8d968b4d823886c2f170a272b478
rockBravo2 <00001116438707sp@al.educacao.sp.gov.br>
2025-09-09 11:05:40 -0300

    i

--- script.js
+++ script.js
@@ -32,3 +32,8 @@ document.body.classList.toggle('alto-contarste');
     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
     botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado);
 })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });

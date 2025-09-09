778e34777fadecc5dbaf4254394abd46845a66c0
rockBravo2 <00001116438707sp@al.educacao.sp.gov.br>
2025-09-09 10:16:30 -0300

    a

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
- const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById('diminuir-fonte');
- 
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-
-    })
-    aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
+     diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
 
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
-botaoDeAcessibilidade.addEventListener('click', function(){
-   botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-   opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-})
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
 
 })
